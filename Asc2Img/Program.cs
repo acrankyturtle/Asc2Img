7881a4ce93c55f378480367a54a082da1034f87a
cranky <crankyturtle@hotmail.com>
2023-07-22 16:08:04 -0400

    Initial commit

---
+++ Asc2Img/Program.cs
@@ -0,0 +1,64 @@
+using System.Diagnostics;
+
+namespace Asc2Img;
+
+public static class Program
+{
+	public static void Main(string[]? args)
+	{
+		var (sourceFile, destinationFile) = args switch
+		{
+			null or [] => (ReadFileName("Source file", true), ReadFileName("Destination file", false)),
+			[var src, var dst] => (src, dst),
+			_
+				=> throw new(
+					"Invalid number of arguments. Either run with zero arguments, or with exactly two, where the first is the source file and the second is the destination file."
+				),
+		};
+
+		Console.WriteLine();
+		Console.Write("Working... ");
+
+		var stopWatch = Stopwatch.StartNew();
+
+		// open source file
+		using var fileStream = new FileStream(sourceFile, FileMode.Open, FileAccess.Read, FileShare.Read);
+		using var reader = new StreamReader(fileStream);
+
+		// read esri data
+		var esri = EsriReader.Read(reader);
+
+		// convert esri data to image
+		var bitmap = ImageFromSamples.FromValues(esri.Values);
+
+		// write to destination
+		bitmap.Save(destinationFile);
+
+		stopWatch.Stop();
+
+		Console.WriteLine("done.");
+		Console.WriteLine(
+			$"Completed {esri.Header.ColumnCount}x{esri.Header.RowCount} map in {stopWatch.Elapsed.TotalSeconds:n} seconds."
+		);
+		Console.WriteLine(
+			$"({esri.Header.ColumnCount * esri.Header.RowCount / stopWatch.Elapsed.TotalSeconds:n} pixels per second)"
+		);
+	}
+
+	private static string ReadFileName(string prompt, bool mustExist)
+	{
+		while (true)
+		{
+			Console.Write(prompt);
+			Console.Write(": ");
+
+			var fileName = Path.GetFullPath(Console.ReadLine() ?? throw new FileNotFoundException());
+
+			if (!mustExist || File.Exists(fileName))
+				return fileName;
+
+			Console.WriteLine("File does not exist.");
+			Console.WriteLine();
+		}
+	}
+}
---
+++ Asc2Img/README.md
@@ -0,0 +1,11 @@
+# Asc2Img
+A simple command-line tool to convert ASCII ESRI elevation data into a 2d heightmap. 
+
+## How to
+You can either run the program with no arguments and use the prompts, or specify the source and destination files like this:
+
+```shell
+asc2img path/to/my/sourceFile.asc path/to/my/destinationFile.png
+```
+
+Asc2Img supports a number of popular image file formats, such as `png`, `jpg/jpeg`, `tiff`, `gif`, and more.
